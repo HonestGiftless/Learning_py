fde3134025dc4777d1d8ce0aeb3060276a28ebe7
HonestGiftless <113893391+HonestGiftless@users.noreply.github.com>
2023-02-21 13:19:07 +0300

    Create aver_num.py

---
+++ elifs/aver_num.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+if a > b and b > c:
+    print(b)
+elif a > b and b < c and a > c:
+    print(c)
+elif b > a and a > c:
+    print(a)
+elif b > a and a < c and b > c:
+    print(c)
+elif c > a and a > b:
+    print(a)
+elif c > a and a < b and c > b:
+    print(b)
