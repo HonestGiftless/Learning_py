e404fb5eab1f4e6347830ac7c8af32f94fba7f8b
HonestGiftless <113893391+HonestGiftless@users.noreply.github.com>
2023-02-21 23:59:22 +0800

    Create three_sorted.py

---
+++ data types/three_sorted.py
@@ -0,0 +1,11 @@
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+
+summ = n1+n2+n3
+minimal = min(n1,n2,n3)
+maximal = max(n1,n2,n3)
+
+print(maximal)
+print(summ - (minimal+maximal))
+print(minimal)
