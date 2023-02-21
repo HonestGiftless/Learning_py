d00cb0c044570bd2fa629365ae361f05cfd771eb
HonestGiftless <113893391+HonestGiftless@users.noreply.github.com>
2023-02-22 00:04:48 +0800

    Create name.py

---
+++ string types/name.py
@@ -0,0 +1,8 @@
+#Напишите программу, которая считывает с клавиатуры две строки – имя и фамилию пользователя и выводит фразу:
+
+#             «Hello [введенное имя] [введенная фамилия]! You just delved into Python».
+
+fn = input('Enter ur name: ')
+sn = input('Enter ur surname: ')
+
+print('Hello ' + fn + ' ' + sn + '! You just delved into Python')
