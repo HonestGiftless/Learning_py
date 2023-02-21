d5fcb8eec61ad20ae8a081f73d6628dfbc9a2518
HonestGiftless <113893391+HonestGiftless@users.noreply.github.com>
2023-02-21 15:44:17 +0800

    Create comp_price.py

---
+++ arithmetic/comp_price.py
@@ -0,0 +1,6 @@
+monitor = int(input('Введите цену монитора: '))
+sys_block = int(input('Введите цену системного блока: '))
+keyboard = int(input('Введите цену клавиатуры: '))
+mouse = int(input('Введите цену мыши: '))
+
+print(3 * (monitor + sys_block + keyboard + mouse))
